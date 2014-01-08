60129a3de2fbfe17446a45fbca3620977396e69f
wilsaj <wilson.andrew.j+github@gmail.com>
2014-01-07 18:19:30 -0600

    clean up parse_records() method a bit

--- sdi/binary.py
+++ sdi/binary.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import itertools
 import struct
 from StringIO import StringIO
 
@@ -139,56 +140,7 @@ class Dataset(object):
         }
 
     def parse_records(self, fid, data_length):
-        pre_struct, post_struct = self.record_struct()
-        event_struct = [('event', None, None)]
-
-        # intitialize trace lists
-        trace_metadata = dict([
-            [name, []] for name, fmt, dtype in pre_struct + post_struct + event_struct
-        ])
-
-        trace_intensities = []
-
-        # pre-compute format, names and size for unpacking
-        pre_fmt, pre_names, pre_size = self._split_struct_list(pre_structs)
-        post_fmt, post_names, post_size = self._split_struct_list(post_structs)
-
-        npos = 12
-        fid.seek(npos)
-
-        while npos < data_length:
-            pre_record = struct.unpack(pre_fmt, fid.read(pre_size))
-            pre_dict = dict(zip(pre_names, pre_record))
-
-            for key, value in pre_dict.iteritems():
-                trace_metadata[key].append(value)
-
-            size = pre_dict['event_len']
-            if size > 0:
-                event = struct.unpack('<' + str(size) + 's', fid.read(size))[0]
-            else:
-                event = ''
-            trace_metadata['event'].append(event)
-
-            post_record = struct.unpack(post_fmt, fid.read(post_size))
-            post_dict = dict(zip(post_names, post_record))
-            for key, value in post_dict.iteritems():
-                trace_metadata[key].append(value)
-
-            fid.seek(npos + pre_dict['offset'] + 2)
-            size = pre_dict['num_pnts']
-            trace_intensities.append(struct.unpack('<' + str(size) + 'H', fid.read(size * 2)))
-            npos = int(fid.tell())
-
-        # convert trace_metadata lists to np arrays
-        for key, value, dtype in pre_struct + post_struct + event_struct:
-            trace_metadata[key] = np.array(trace_metadata[key], dtype=dtype)
-        self.trace_metadata = trace_metadata
-
-        self.intensity_image = self.build_intensity_image(trace_intensities)
-
-    def record_struct(self):
-        pre_struct = [
+        pre_structs = [
             ('offset', 'H', np.int),
             ('trace_num', 'l', np.int),
             ('units', 'B', np.int),
@@ -209,42 +161,84 @@ class Dataset(object):
             ('num_pnts','h', np.int),
             ('clock', 'l', np.int),
             ('hour', 'B', np.int),
-            ('min', 'B', np.int),
-            ('sec', 'B', np.int),
-            ('sec100', 'B', np.int),
+            ('minute', 'B', np.int),
+            ('second', 'B', np.int),
+            ('centisecond', 'B', np.int),
             ('rate', 'l', np.int),
             ('kHz', 'f', np.float),
             ('event_len', 'B', np.int),
         ]
-
-        post_struct = [
+        event_struct = [('event', None, None)]
+        post_structs = [
             ('longitude', 'd', np.float64),
             ('latitude', 'd', np.float),
             ('transducer', 'B', np.int),
             ('options', 'B', np.int),
             ('data_offset', 'B', np.int),
         ]
-
         if self.version >= '3.3':
-            post_struct.append(('utm_x', 'd', np.float))
-            post_struct.append(('utm_y', 'd', np.float))
-
+            post_structs.append(('utm_x', 'd', np.float))
+            post_structs.append(('utm_y', 'd', np.float))
         if self.version >= '4.0':
-            post_struct.append(('cycles', 'B', np.int))
-            post_struct.append(('volts', 'B', np.int))
-            post_struct.append(('power', 'B', np.int))
-            post_struct.append(('gain', 'B', np.int))
-            post_struct.append(('previous_offset', 'H', np.int16))
-
+            post_structs.append(('cycles', 'B', np.int))
+            post_structs.append(('volts', 'B', np.int))
+            post_structs.append(('power', 'B', np.int))
+            post_structs.append(('gain', 'B', np.int))
+            post_structs.append(('previous_offset', 'H', np.int16))
         if self.version >= '4.2':
-            post_struct.append(('antenna_e1', '4f', np.float))
-            post_struct.append(('antenna_ht', '4f', np.float))
-            post_struct.append(('draft', '4f', np.float))
-            post_struct.append(('tide', '4f', np.float))
-
+            post_structs.append(('antenna_e1', '4f', np.float))
+            post_structs.append(('antenna_ht', '4f', np.float))
+            post_structs.append(('draft', '4f', np.float))
+            post_structs.append(('tide', '4f', np.float))
         if self.version >= '4.3':
-            post_struct.append(('gps_mode', 'B', np.int))
-            post_struct.append(('hdop', 'f', np.float))
+            post_structs.append(('gps_mode', 'B', np.int))
+            post_structs.append(('hdop', 'f', np.float))
+
+        all_structs = pre_structs + event_struct + post_structs
+
+        # intitialize dict of trace elements
+        raw_trace = dict([
+            [name, []] for name, fmt, dtype in all_structs
+        ])
+
+        trace_intensities = []
+
+        # pre-compute format, names and size for unpacking
+        pre_fmt, pre_names, pre_size = self._split_struct_list(pre_structs)
+        post_fmt, post_names, post_size = self._split_struct_list(post_structs)
+
+        npos = 12
+        fid.seek(npos)
+
+        # loop through data extract traces. the length of the event string
+        # changes between traces and is defined just before it (event_len), so
+        # parsing a line is split into three stages: pre-event, event, and
+        # post-event
+        while npos < data_length:
+            pre_record = struct.unpack(pre_fmt, fid.read(pre_size))
+            pre_dict = dict(zip(pre_names, pre_record))
+
+            size = pre_dict['event_len']
+            if size > 0:
+                event = struct.unpack('<' + str(size) + 's', fid.read(size))[0]
+            else:
+                event = ''
+            raw_trace['event'].append(event)
+
+            post_record = struct.unpack(post_fmt, fid.read(post_size))
+            post_dict = dict(zip(post_names, post_record))
+
+            for key, value in itertools.chain(pre_dict.iteritems(), post_dict.iteritems()):
+                raw_trace[key].append(value)
+
+            fid.seek(npos + pre_dict['offset'] + 2)
+
+            size = pre_dict['num_pnts']
+            intensity = struct.unpack('<' + str(size) + 'H', fid.read(size * 2))
+            trace_intensities.append(intensity)
+            npos = int(fid.tell())
+
+        self.intensity_image = self.build_intensity_image(trace_intensities)
 
     def _split_struct_list(self, struct_list):
         """Helper method for splitting struct lists into components for
